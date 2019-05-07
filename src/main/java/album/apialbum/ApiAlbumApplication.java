31287abe248236fd2102a0b1d7e3076df692c08f
Agustin Servat <agustinservat@gmail.com>
2019-05-06 22:20:23 -0300

    First commit

---
+++ src/main/java/album/apialbum/ApiAlbumApplication.java
@@ -0,0 +1,13 @@
+package album.apialbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/album/apialbum/ApiAlbumApplicationTests.java
@@ -0,0 +1,16 @@
+package album.apialbum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiAlbumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
